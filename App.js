acea0f999ce8044f621ee7ee94be94f8e38af306
ssoferret <owosso1212@gmail.com>
2024-06-03 17:19:09 +0900

    🚩 Add router import route and app.use

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
-// app.listen(9999);
+app.listen(9999);
 
 const userRouter = require('./routes/users');
+// const bookRouter = require('./routes/books');
+// const cartRouter = require('./routes/carts');
+// const categoryRouter = require('./routes/categories');
+// const likeRouter = require('./routes/likes');
+// const orderRouter = require('./routes/orders');
 
-userRouter();
+app.use('/users', userRouter);
+// app.use('/books', bookRouter);
+// app.use('/carts', cartRouter);
+// app.use('/category', cartegoryRouter);
+// app.use('/likes', likeRouter);
+// app.use('/orders', orderRouter);
