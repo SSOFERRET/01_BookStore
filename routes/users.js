d09c8d01f1ad517dc1e0918d5b02a186972259ed
ssoferret <owosso1212@gmail.com>
2024-06-03 16:13:37 +0900

    ✨ files for routes create

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+// const router = express.Router();
+// const conn = require('./../mariadb');
+
+// router.use(express.json);
+
+const router = () => {
+    console.log('usersRoute');
+}
+
+module.exports = router;
