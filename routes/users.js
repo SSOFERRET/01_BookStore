5dbb84a39fa014e6b52f25f530fd18a70c9e6c0d
ssoferret <owosso1212@gmail.com>
2024-06-04 12:57:04 +0900

    🐛 express.json brackets miss

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { join } = require('./../controllers/UserController');
 
-router.use(express.json);
+router.use(express.json());
 
 router.post('/join', join);
 
