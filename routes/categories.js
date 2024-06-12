5c54b0187ba6aa6c53f8b5a799bc7656b43ca5f5
ssoferret <owosso1212@gmail.com>
2024-06-12 17:45:29 +0900

    🐛 통신 에러 발생 원인 수정: express.json()에 괄호 누락

--- routes/categories.js
+++ routes/categories.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {getAllCategory} = require('./../controllers/CategoryController')
 
-router.use(express.json);
+router.use(express.json());
 
 router.get('', getAllCategory);
 
