31347f7a940a5542833e3b6d89370a6e0cb95c19
ssoferret <owosso1212@gmail.com>
2024-06-12 17:44:39 +0900

    🩹 테이블명에 데이터베이스 명 덧붙임

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -2,14 +2,14 @@ const conn = require('./../mariadb');
 const {StatusCodes} = require('http-status-codes');
 
 const getAllCategory = (_, res) => {
-    let sql = 'SELECT * FROM categories';
+    let sql = 'SELECT * FROM BookStore.categories';
     conn.query(sql, 
         (err, results) => {
             if (err) {
                 console.log(err);
                 return;
             }
-            res.status(StatusCodes.OK).json({results});
+            return res.status(StatusCodes.OK).json(results);
         }
     )
 };
