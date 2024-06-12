9b413dda54113b8b43474e49782428e335117e87
ssoferret <owosso1212@gmail.com>
2024-06-12 16:50:58 +0900

    ➕ cors 연동 작성

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.listen(9999);
 
@@ -9,6 +10,16 @@ const cartRouter = require('./routes/carts');
 const categoryRouter = require('./routes/categories');
 const likeRouter = require('./routes/likes');
 const orderRouter = require('./routes/orders');
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 204,
+    credential: true
+};
+
+app.use(cors(corsOptions));
 
 app.use('/users', userRouter);
 app.use('/books', bookRouter);
